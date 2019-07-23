b62911785f49cf767c93ac0301996abde3c2d152
Raffi Shahbazian <raffi.p.shahbazian@gmail.com>
2019-07-23 13:39:56 -0700

    red/blue home bases

--- src/components/Gameboard.jsx
+++ src/components/Gameboard.jsx
@@ -5,10 +5,23 @@ let topOfGameboardCSS = {
   borderRadius: "0 0 50px 50px",
   height: "45vh"
 };
+let topOfGameboardCSSAlt = {
+  padding: '5px',
+  border: "thick ridge #039be5",
+  borderRadius: "0 0 50px 50px",
+  height: "45vh"
+};
 
 let bottomOfGameboardCSS = {
   padding: '5px',
   border: "thick ridge #795548",
+  borderRadius: "50px 50px 0 0",
+  height: "45vh"
+};
+
+let bottomOfGameboardCSSAlt = {
+  padding: '5px',
+  border: "thick ridge #bf360c",
   borderRadius: "50px 50px 0 0",
   height: "45vh"
 };
@@ -20,8 +33,7 @@ export default function Gameboard({ gameState }) {
       {gameState.map(space => {
         if (space.id > 12) {
           return (
-
-            <div className="col-1 d-flex flex-column" style={topOfGameboardCSS}>
+            <div className="col-1 d-flex flex-column" style={space.id < 19 ? topOfGameboardCSS : topOfGameboardCSSAlt}>
               {[...Array(space.count)].map(x => {
                 return(
                   <button className={space.color === 1 ? "btn btn-sm btn-primary" : "btn btn-sm btn-danger"}>
@@ -40,7 +52,7 @@ export default function Gameboard({ gameState }) {
         if (space.id < 13){
           return (
             <div
-              className="col-1 d-flex flex-column-reverse" style={bottomOfGameboardCSS}>
+              className="col-1 d-flex flex-column-reverse" style={space.id > 6 ? bottomOfGameboardCSS : bottomOfGameboardCSSAlt}>
               {[...Array(space.count)].map(x => {
                 return(
                   <button className={space.color === 1 ? "btn btn-sm btn-primary" : "btn btn-sm btn-danger"}>

64991c83378bc990d4ed4d5f695029790ca77614
Raffi Shahbazian <raffi.p.shahbazian@gmail.com>
2019-07-23 11:35:32 -0700

    css gameboard

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
-
+import Gameboard from './components/Gameboard'
 function App() {
   return (
     <div className="App">
-
+      <Gameboard />
     </div>
   );
 }
---
+++ src/components/Gameboard.jsx
@@ -0,0 +1,98 @@
+import React from 'react'
+
+let topOfGameboardCSS = {
+  border: 'thick solid #0000FF',
+  borderRadius: '0 0 20px 20px',
+  height: '50vh'
+}
+
+let bottomOfGameboardCSS = {
+  border: 'thick solid #00ffff',
+  borderRadius: '20px 20px 0 0',
+  height: '50vh'
+
+}
+
+
+export default function Gameboard() {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-1" style={topOfGameboardCSS}>
+          hi
+        </div>
+        <div className="col-1" style={topOfGameboardCSS}>
+          hi
+        </div>
+        <div className="col-1" style={topOfGameboardCSS}>
+          hi
+        </div>
+        <div className="col-1" style={topOfGameboardCSS}>
+          hi
+        </div>
+        <div className="col-1" style={topOfGameboardCSS}>
+          hi
+        </div>
+        <div className="col-1" style={topOfGameboardCSS}>
+          hi
+        </div>
+        <div className="col-1" style={topOfGameboardCSS}>
+          hi
+        </div>
+        <div className="col-1" style={topOfGameboardCSS}>
+          hi
+        </div>
+        <div className="col-1" style={topOfGameboardCSS}>
+          hi
+        </div>
+        <div className="col-1" style={topOfGameboardCSS}>
+          hi
+        </div>
+        <div className="col-1" style={topOfGameboardCSS}>
+          hi
+        </div>
+        <div className="col-1" style={topOfGameboardCSS}>
+          hi
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-1 d-flex align-items-end" style={bottomOfGameboardCSS}>
+          hi
+        </div>
+        <div className="col-1 d-flex align-items-end" style={bottomOfGameboardCSS}>
+          hi
+        </div>
+        <div className="col-1 d-flex align-items-end" style={bottomOfGameboardCSS}>
+          hi
+        </div>
+        <div className="col-1 d-flex align-items-end" style={bottomOfGameboardCSS}>
+          hi
+        </div>
+        <div className="col-1 d-flex align-items-end" style={bottomOfGameboardCSS}>
+          hi
+        </div>
+        <div className="col-1 d-flex align-items-end" style={bottomOfGameboardCSS}>
+          hi
+        </div>
+        <div className="col-1 d-flex align-items-end" style={bottomOfGameboardCSS}>
+          hi
+        </div>
+        <div className="col-1 d-flex align-items-end" style={bottomOfGameboardCSS}>
+          hi
+        </div>
+        <div className="col-1 d-flex align-items-end" style={bottomOfGameboardCSS}>
+          hi
+        </div>
+        <div className="col-1 d-flex align-items-end" style={bottomOfGameboardCSS}>
+          hi
+        </div>
+        <div className="col-1 d-flex align-items-end" style={bottomOfGameboardCSS}>
+          hi
+        </div>
+        <div className="col-1 d-flex align-items-end" style={bottomOfGameboardCSS}>
+          hi
+        </div>
+      </div>
+    </div>
+  )
+}

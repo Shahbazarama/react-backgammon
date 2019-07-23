0f190889115de90068387fb21cf2d72610df63cd
Raffi Shahbazian <raffi.p.shahbazian@gmail.com>
2019-07-23 12:27:06 -0700

    gameboard set up

--- src/components/Gameboard.jsx
+++ src/components/Gameboard.jsx
@@ -7,41 +7,48 @@ let topOfGameboardCSS = {
 
 let bottomOfGameboardCSS = {
   border: "thick solid #00ffff",
-  borderWidth: "0 38.5px 300px 38.5px",
+  borderRadius: "20px 20px 0 0",
   height: "50vh"
 };
 
 export default function Gameboard({ gameState }) {
   return (
     <>
+    <div className="row">
       {gameState.map(space => {
         if (space.id > 12) {
           return (
-            <div className="row">
-              return (
-              <div className="col-1" style={topOfGameboardCSS}>
-                {[...Array(space.count)].map(x => {
-                  return "hi";
-                })}
-              </div>
-              );
-            </div>
-          );
-        } else {
-          return (
-            <div className="row">
-              <div
-                className="col-1 d-flex align-items-end"
-                style={bottomOfGameboardCSS}
-              >
-                {[...Array(space.count)].map(x => {
-                  return "hi";
-                })}
-              </div>
+            <div className="col-1" style={topOfGameboardCSS}>
+              {[...Array(space.count)].map(x => {
+                return(
+                  <button className={space.color == 1 ? "btn btn-sm btn-primary" : "btn btn-sm btn-danger"}>
+                    piece
+                  </button>
+                )
+              })}
             </div>
           );
         }
       })}
-    </>
+    </div>
+    <div className="row">
+      {gameState.slice(0).reverse().map(space => {
+        if (space.id < 13){
+          return (
+            <div
+              className="col-1 d-flex flex-column-reverse align-items-end" style={bottomOfGameboardCSS}>
+              {[...Array(space.count)].map(x => {
+                return(
+                  <button className={space.color == 1 ? "btn btn-sm btn-primary" : "btn btn-sm btn-danger"}>
+                    piece
+                  </button>
+                )
+              })}
+            </div>
+          )
+        }
+      })}
+    </div>
+  </>
   );
 }

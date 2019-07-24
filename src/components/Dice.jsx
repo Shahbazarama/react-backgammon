ad46db9f8fe5a714ec22b139ddf38d3efc7ce5a4
Raffi Shahbazian <raffi.p.shahbazian@gmail.com>
2019-07-24 14:06:32 -0700

    fresh dice icons, npm i

--- src/components/Dice.jsx
+++ src/components/Dice.jsx
@@ -1,5 +1,9 @@
 import React from 'react'
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faDiceOne, faDiceTwo, faDiceThree, faDiceFour, faDiceFive, faDiceSix } from '@fortawesome/free-solid-svg-icons'
+
+
 
 export default function Dice({ diceValues, rollDice, whosTurn }) {
   let diceArray = []
@@ -8,15 +12,20 @@ export default function Dice({ diceValues, rollDice, whosTurn }) {
       diceArray.push(value)
     }
   }
+
   return (
     <div>
       <div>
-        <i className="fas fa-camera fa-lg"></i>
-
-
         {diceArray.map(roll => {
           return (
-            <h1>{roll}</h1>
+            <>
+              {roll == 1 ? <FontAwesomeIcon icon={faDiceOne} size="4x" /> : <></>}
+              {roll == 2 ? <FontAwesomeIcon icon={faDiceTwo} size="4x" /> : <></>}
+              {roll == 3 ? <FontAwesomeIcon icon={faDiceThree} size="4x" /> : <></>}
+              {roll == 4 ? <FontAwesomeIcon icon={faDiceFour} size="4x" /> : <></>}
+              {roll == 5 ? <FontAwesomeIcon icon={faDiceFive} size="4x" /> : <></>}
+              {roll == 6 ? <FontAwesomeIcon icon={faDiceSix} size="4x" /> : <></>}
+            </>
           )
         })}
         <h1>{diceArray.length === 0 ? "Next Turn" : ""}</h1>
@@ -24,6 +33,7 @@ export default function Dice({ diceValues, rollDice, whosTurn }) {
       <button className="btn btn-lg btn-info" onClick={() => rollDice()}>
         Roll the Dice!
       </button>
+
 
       <h2>{whosTurn === 1 ? "Blue's Turn" : "Red's Turn"}</h2>
     </div>

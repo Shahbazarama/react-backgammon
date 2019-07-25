74ae0d0bf99e1d8e947f3af0cac58350b54eaa5f
Raffi Shahbazian <raffi.p.shahbazian@gmail.com>
2019-07-25 12:10:25 -0700

    Updates

---
+++ precache-manifest.002fc03e272c554af680ed15f62beafe.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a839e07d819221ec27190742d5ef905",
+    "url": "/react-backgammon/index.html"
+  },
+  {
+    "revision": "f0e7acc7c32257cd7098",
+    "url": "/react-backgammon/static/css/2.fc3aa306.chunk.css"
+  },
+  {
+    "revision": "0e1a0cf08f6e3cd0a6a6",
+    "url": "/react-backgammon/static/css/main.d8b8f783.chunk.css"
+  },
+  {
+    "revision": "f0e7acc7c32257cd7098",
+    "url": "/react-backgammon/static/js/2.b36ca552.chunk.js"
+  },
+  {
+    "revision": "0e1a0cf08f6e3cd0a6a6",
+    "url": "/react-backgammon/static/js/main.c04b4475.chunk.js"
+  },
+  {
+    "revision": "f343d7a9036394b427ca",
+    "url": "/react-backgammon/static/js/runtime~main.00eac9c1.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/react-backgammon/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/react-backgammon/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/react-backgammon/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/react-backgammon/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/react-backgammon/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
